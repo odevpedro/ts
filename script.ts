072952a8d201d33e7a13c23ee59c440bd6dfde51
odevpedro <pedrosschmidt2@gmail.com>
2023-03-09 23:45:40 -0300

    initial files

---
+++ script.ts
@@ -0,0 +1,19 @@
+
+//O tipo define aquilo que podemos fazer com os dados
+
+const frase = 'Front End';
+const total = 100.05;
+const empresas = ['Apple', 'Microsoft']
+//const body = document.body;
+//const button = document.querySelector('button');
+
+frase.charCodeAt(2);
+
+var oi = frase.toLowerCase();
+console.log(oi)
+
+total.toFixed();
+
+empresas.map((empresa) => empresa.toLowerCase());
+
+///body.style.background = '#000';

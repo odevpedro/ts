48049cee0c6e41d00b07b51f3630a54f5061f461
odevpedro <pedrosschmidt2@gmail.com>
2023-03-10 15:36:42 -0300

    ts tools

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-//@ts-check
-const produtos = [
-    { nome: 'O senhor dos anéis', tipo: 'livro' },
-    { nome: 'Harry Potter e a Pedra Filosofal', tipo: 'livro' },
-    { nome: 'Iphone XR', tipo: 'eletrônico' }
-  ]
-
-  function filtraLivros(dados){
-    return dados.filter((item) => item.tipo === 'livro' );
-  }
-
-  console.log(filtraLivros(produtos));
+"use strict";
+const total = 100;
+function somar(a, b) {
+    return a + b;
+}
+console.log(somar(5, 10));

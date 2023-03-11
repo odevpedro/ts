e5820d661117db8eef121392412d9ae13c8dc2fe
odevpedro <pedrosschmidt2@gmail.com>
2023-03-11 13:37:03 -0300

    inference

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
-const total = 100;
-function somar(a, b) {
-    return a + b;
-}
-console.log(somar(5, 10));
+const produto = "Livro";
+const preco = 200;
+const carro = {
+    marca: "Audi",
+    portas: 5,
+};
+carro.marca = "Suzuki";
+// Inference O ts consegue inferir o tipo de dados de expressões em javascript. Sempre que ele conseguir inferir, você não precisa fazer a anotação do dado
+//inferencia seria fazer uma leitura do js sem executa-lo
+const barato = preco < 400 ? true : 'produto caro';
--- script.ts
+++ script.ts
@@ -1,7 +1,20 @@
-const total = 100;
+const produto: string = "Livro";
+const preco: number = 200;
 
-function somar(a: number, b: number){
-  return a + b
+const carro: {
+  marca: string;
+  portas: number;
+} = 
+{
+  marca: "Audi",
+  portas: 5,
 }
-console.log(somar(5, 10))
 
+carro.marca = "Suzuki"
+
+
+// Inference O ts consegue inferir o tipo de dados de expressões em javascript. Sempre que ele conseguir inferir, você não precisa fazer a anotação do dado
+
+//inferencia seria fazer uma leitura do js sem executa-lo
+
+const barato = preco < 400 ? true : 'produto caro'; 

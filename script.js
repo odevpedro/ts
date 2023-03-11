e72f5505b2335419438300249625aae0cfad5bd7
odevpedro <pedrosschmidt2@gmail.com>
2023-03-11 15:38:08 -0300

    fixed code

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function normalizarTexto(texto) {
-    return texto.trim().toLowerCase();
+var input = document.querySelector("input");
+var total = localStorage.getItem("total");
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
 }
-console.log(normalizarTexto(' D desing'));
+function calcularGanho(value) {
+    var p = document.querySelector("p");
+    if (p) {
+        p.innerText = "ganho total: ".concat(value + 100 - value * 0.2);
+    }
+}
+function totalMudou() {
+    if (input) {
+        localStorage.setItem("total", input.value);
+        calcularGanho(Number(input.value));
+    }
+    //Salvando informações pós atualização da pagina
+}
+if (input) {
+    input.addEventListener("keyup", totalMudou);
+}
--- script.ts
+++ script.ts
@@ -1,6 +1,33 @@
+const input = document.querySelector("input");
 
-function normalizarTexto(texto: string){
-  return texto.trim().toLowerCase();
+const total = localStorage.getItem("total");
+
+if(input && total){
+  input.value = total;
+  calcularGanho(Number(input.value));
 }
 
-console.log(normalizarTexto(' D desing'));
+
+function calcularGanho(value: number) {
+    const p = document.querySelector("p");
+    if(p){
+    p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+  }
+}
+
+function totalMudou(){
+  if(input){
+
+    localStorage.setItem("total", input.value);
+    calcularGanho(Number(input.value))
+  }
+
+
+    //Salvando informações pós atualização da pagina
+
+}
+
+if(input){
+  input.addEventListener("keyup", totalMudou);
+}
+
